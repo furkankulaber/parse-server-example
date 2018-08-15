91b859b795785baef6cadbafd091f1b88a44fe02
FURKAN KULABER <furkankulaber@clemury.com>
2018-08-15 20:09:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_pdpvzpl4:61l75rd1oqkpi7fj45tc6no2fe@ds123372.mlab.com:23372/heroku_pdpvzpl4',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'GfwMS3n3mHC7YiDGleHSIIQwv45Mzwi2u8GiB8EQ',
+  masterKey: process.env.MASTER_KEY || 'JGyWz1JttekEZM7Kt37KY32fEzhM4ND7oeM8q9B4', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
